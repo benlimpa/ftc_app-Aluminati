cb91f40428c40900fec51f246e696fcdd9d0b9d8
nylatiki <benmomo1@gmail.com>
2016-01-06 16:02:33 -0800

    Created TeleOp Class

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RobotTeleOp.java
@@ -0,0 +1,13 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by BenL on 1/6/16.
+ */
+public class RobotTeleOp extends RobotTelemetry
+{
+    public RobotTeleOp ()
+    {
+        super(false); // Do not use drive encoders
+    }
+
+}
